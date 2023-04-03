5ff841d009ef4c7494a5014c17d7340b6de90aca
Josh Burch <joshuaburch01@gmail.com>
2023-04-03 15:14:38 -0500

    fixed functions

--- evaluate.py
+++ evaluate.py
@@ -15,7 +15,7 @@ from sklearn.metrics import r2_score
 def plot_residuals(y, yhat):
     residuals = y - yhat
     
-    plt.scatter(residuals, y_hat)
+    plt.scatter(residuals, y_hat )
     plt.xlabel('Residuals')
     plt.ylabel("tax_value")
     plt.title("prediction Model")
@@ -27,12 +27,18 @@ def regression_errors(y, yhat):
     residuals = y - yhat
     baseline = round(y.mean(), 2)
     baseline_residuals = y - baseline
+    SSE = (residuals**2).sum()
+    TSS = (baseline_residuals**2).sum()
+    ESS = TSS - SSE
+    MSE = SSE/len(y)
+    RMSE = MSE**.5
+    
     values = {
-        'SSE' : (residuals**2).sum(),
-        'TSS' : (baseline_residuals**2).sum(),
-        'ESS' : TSS - SSE,
-        'MSE' : SSE/len(y),
-        'RMSE' : MSE**.5   
+        'SSE' : SSE,
+        'TSS' : TSS,
+        'ESS' : ESS,
+        'MSE' : MSE,
+        'RMSE' : RMSE   
         }
     return values
     
@@ -41,23 +47,25 @@ def regression_errors(y, yhat):
 def baseline_mean_errors(y):
     baseline = round(y.mean(), 2)
     residuals = y - baseline
+    SSE =(residuals**2).sum()
+    MSE = SSE/len(y)
+    RMSE = MSE**.5 
     values = {
-        'SSE' : (residuals**2).sum(),
-        'MSE' : SSE/len(y),
-        'RMSE' : MSE**.5   
+        'SSE' : SSE,
+        'MSE' : MSE,
+        'RMSE' : RMSE 
         }
-    
     return values
 
 
 
 def better_than_baseline(y, yhat):
-    model_score = r2_score(y, yhat)
-    baseline = round(y.mean(), 2)
-    baseline_arr = np.full(y.shape , baseline)
-    baseline_score = r2_score(y, baseline_arr)
-    if model_score > baseline_score:
+def better_than_baseline(y, yhat):
+    model_score = regression_errors(y, yhat)['RMSE']
+    baseline_score = baseline_mean_errors(y)['RMSE']
+    if model_score < baseline_score:
         return True
     else: 
         return False
+    
     
